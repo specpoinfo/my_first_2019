65a66ae41bb07ea4b11dcac63608ea38bf6ccd8c
specpoinfo <subbotin1818@gmail.com>
2020-01-01 22:33:07 +0300

    Added new file for working with tKinter

---
+++ newtesting.py
@@ -0,0 +1,22 @@
+from tkinter import Tk, Label, Button, Entry
+
+def clicked():
+    res = "Привет, {}".format(txt.get())
+    lbl.configure(text=res)
+
+
+window = Tk()
+window.title("Добро пожаловать в приложение PythonRu")
+window.geometry('800x350')
+lbl = Label(window, text="Привет",)
+lbl.grid(column=0, row=0)
+txt = Entry(window, width=10)
+txt.grid(column=1, row = 0)
+btn = Button(window, text="Клик", command=clicked)
+btn.grid(column=2, row=0)
+
+txt.focus()
+
+
+
+window.mainloop()
